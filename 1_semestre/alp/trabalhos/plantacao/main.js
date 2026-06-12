3e09103fb6cb1156e6e765493298afa7ea3a8356
Vitor Gabriel Martins Blan <160440604+VitorBlan@users.noreply.github.com>
2026-06-11 22:05:29 -0400

    Add game initialization and welcome message

--- 1_semestre/alp/trabalhos/plantacao/main.js
+++ 1_semestre/alp/trabalhos/plantacao/main.js
@@ -2,7 +2,26 @@ import escreva from "./src/saida.js";
 import leia from "./src/entrada.js";
 import chalk from "chalk";
 
+import { iniciarJogo } from "./sala.js";
+
 escreva(chalk.bgBlue("Comando de saída de dados..."));
 let nome = leia(chalk.green("Comando para entrada de dados...Nome: "));
 
-escreva("Seu nome é", nome);
+escreva("Seja bem vindo a Fazenda", nome);
+
+escreva(`
+───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
+───█▒▒░░░░░░░░░▒▒█───
+────█░░█░░░░░█░░█────
+─▄▄──█░░░▀█▀░░░█──▄▄─
+█░░█─▀▄░░░░░░░▄▀─█░░█
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+█░░║║║╠─║─║─║║║║║╠─░░█
+█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+
+`);
+
+// Inicia o loop de navegação do jogo contido em sala.js
+iniciarJogo();

4819a453b85c708d8fb4a4db62f67a2dc1a6f0a2
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-17 14:42:01 -0400

    Crrd funcao guardar item

--- 1_semestre/alp/trabalhos/plantacao/src/inventorio/guardarItem.js
+++ 1_semestre/alp/trabalhos/plantacao/src/inventorio/guardarItem.js
@@ -2,6 +2,6 @@ import inventorio from "./inventorio.js";
 import { ambientes } from "../../ambientes/index.js";
 
 export default function guardar(item, local) {
-    inventorio.slice(inventorio.indexOf(item), 1);
+    inventorio.splice(inventorio.indexOf(item), 1);
     ambientes[local].items.push(item);
 }

08bb77f3d453f55c646c5988b4336ddb626caefd
Hugo Brandao - IFRO <hugobrandao.dev@gmail.com>
2026-06-17 19:49:19 -0400

    Crrd pegar a vassoura

--- 1_semestre/alp/trabalhos/plantacao/ambientes/casa.js
+++ 1_semestre/alp/trabalhos/plantacao/ambientes/casa.js
@@ -1,5 +1,10 @@
 import escreva from "../src/saida.js";
 import leia from "../src/entrada.js";
+import buscar from "../src/inventorio/buscarItem.js";
+import pegar from "../src/items/pegarItem.js";
+import guardar from "../src/inventorio/guardarItem.js";
+
+let localAtual = "Casa";
 
 export default {
     texto: "Você está na sala de estar da casa. Uma lareira apagada e uma poltrona empoeirada compõem o ambiente.",
@@ -13,6 +18,30 @@ export default {
             const escolha = leia("\nO que você faz? > ");
             if (escolha === "1") return "Dentro há apenas uma conta de luz vencida e um panfleto de pizzaria.";
             return "Você decide não olhar o correio agora.";
+        } },
+        "4": { texto: "Há uma vassoura perto da porta", arte: "vassoura", acao: () => {
+            if (!buscar("vassoura")) {
+                escreva("\nVocê encontrou uma Vassoura!\n", "magenta");
+            
+                let escolha = leia("Deseja pegá-lo? [s/n] ");
+                if (escolha.toLowerCase() === "s") {
+                    if (pegar("vassoura")) {
+                        return "Você pegou a Vassoura.";
+                    }
+                    return "";
+                }
+            } else {
+                escreva("\nVocê está carregando uma vassoura o tempo todo...\n", "magenta");
+
+                let escolha = leia("Deseja colocar a vassoura perto da porta? [s/n] ");
+                if (escolha.toLowerCase() === "s") {
+                    guardar("vassoura", localAtual);
+                    return "Você colocou a Vassoura perto da porta.";
+                }
+            }
+
+            return "Você apenas olhou a vassoura e não fez nada!!!";
         } }
-    }
+    },
+    items: []
 };

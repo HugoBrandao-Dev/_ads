a0cc2b17e2767cac93019d2b38ed9ed7f9824a32
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-09-01 12:39:30 -0400

    Add usuario ou senha invalido.

--- 2_semestre/lp/trabalhos/plantacao_sqlite/src/conta/logar.ts
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/conta/logar.ts
@@ -1,6 +1,7 @@
 import buscarUsuarioSenha from "../db/usuario/buscarUsuarioSenha.ts";
 import leia from "../entrada.js";
 import contaLogada from "./contaLogada.ts";
+import escreva from "../saida.js";
 
 interface Login {
     usuario: string,
@@ -23,6 +24,9 @@ function logar() {
     if (Object.keys(result).length === 1) {
         contaLogada.usua_id = result[0]!.usua_id;
         contaLogada.usua_nome = result[0]!.usua_nome;
+        escreva("\nLogado com sucesso!!", "bgGreen");
+    } else {
+        escreva("\nUsuário ou senha inválido!!", "bgRed");
     }
 }
 
--- 2_semestre/lp/trabalhos/plantacao_sqlite/src/conta/telaLogin.ts
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/conta/telaLogin.ts
@@ -5,12 +5,12 @@ import escreva from "../saida.js";
 
 function login() {
     while (!(contaLogada.usua_id)) {
-        escreva("Você não está logado!!");
 
         let opcao: string = "";
 
         // Verifica se a opção do usuaŕio é um caracter (r ou l)
         while (opcao.length !== 1 || !['l', 'r'].includes(opcao.toLocaleLowerCase())) {
+            console.log();
             opcao = leia("[L]ogar ou [R]egistrar? <l/r>  ");
 
             if (opcao === 'l') {
@@ -19,15 +19,6 @@ function login() {
                 // cadastrar();
             }
         }
-
-    /*
-    const usua_id: number = leia("> ");
-
-    if (usua_id != 0) {
-        contaLogada.usua_id = usua_id;
-        escreva("Logado com sucesso!!");
-    }
-    */
     }
 }
 

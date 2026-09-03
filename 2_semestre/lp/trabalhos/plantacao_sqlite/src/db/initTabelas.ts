68244e76288625a8ff8ac1a01f9afead2133266d
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-09-03 09:33:31 -0400

    Add ddl tabela item

--- 2_semestre/lp/trabalhos/plantacao_sqlite/src/db/initTabelas.ts
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/db/initTabelas.ts
@@ -1,7 +1,9 @@
 import tabelaUsuario from "./usuario/ddl.ts";
+import tabelaItem from "./item/ddl.ts";
 
 function initTabelas(): void {
     tabelaUsuario();
+    tabelaItem();
 }
 
 export default initTabelas;
---
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/db/item/ddl.ts
@@ -0,0 +1,16 @@
+import db from "../conexao.ts";
+// import itemDML from "./dml.ts";
+
+const sql: string = `
+    CREATE TABLE IF NOT EXISTS item(
+        item_id INTEGER PRIMARY KEY NOT NULL,
+        item_nome VARCHAR(100) NOT NULL,
+        item_descricao VARCHAR(255) NOT NULL
+    );
+`
+
+function exec() {
+    db.exec(sql);
+    // itemDML();
+}
+export default exec;

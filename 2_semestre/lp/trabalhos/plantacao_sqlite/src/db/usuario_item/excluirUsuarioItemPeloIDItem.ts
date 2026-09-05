e96d92b17a07ddbd376ac402ff4be28a5bd20d7a
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-09-05 11:33:19 -0400

    Add excluir item usuario_item/inventario

---
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/db/usuario_item/excluirUsuarioItemPeloIDItem.ts
@@ -0,0 +1,18 @@
+import db from "../conexao.ts";
+
+const sql: string = `
+    DELETE FROM usuario_item AS ui WHERE ui.usua_id = ? AND ui.item_id = ?;
+`;
+
+type UsuarioItemID = {
+    usua_id: number,
+    item_id: number
+}
+
+function excluirUsuarioItemPeloIDItem(usuarioItemID: UsuarioItemID): unknown {
+    const select = db.prepare(sql);
+
+    return select.run(usuarioItemID.usua_id, usuarioItemID.item_id);
+}
+
+export default excluirUsuarioItemPeloIDItem;

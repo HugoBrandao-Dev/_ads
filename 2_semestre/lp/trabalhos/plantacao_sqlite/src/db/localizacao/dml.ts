4a91b3f3ddc821a1e3bacf2e86773f018a5aacfd
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-09-06 14:34:40 -0400

    Add ambiente celeiro

--- 2_semestre/lp/trabalhos/plantacao_sqlite/src/db/localizacao/dml.ts
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/db/localizacao/dml.ts
@@ -5,11 +5,12 @@ const sql: string = `
     INSERT INTO localizacao
         (loca_localizacao, loca_descricao)
     VALUES
-        ('Casa Grande', 'Edificação principal da fazenda, destinada à moradia dos proprietários e recepção de hóspedes'),
+        ('Casa', 'Edificação principal da fazenda, destinada à moradia dos proprietários e recepção de hóspedes'),
         ('Celeiro', 'Estrutura coberta utilizada para armazenar colheitas, suprimentos, ferramentas e abrigar animais'),
         ('Horta', 'Área destinada ao cultivo de hortaliças, legumes, ervas aromáticas e temperos para consumo'),
-        ('Pasto 1', 'Área de pastagem delimitada, coberta por vegetação destinada à alimentação e ao manejo do gado'),
-        ('Pasto 2', 'Setor secundário de pastagem para rotação do rebanho e conservação do solo');
+        ('Pasto01', 'Área de pastagem delimitada, coberta por vegetação destinada à alimentação e ao manejo do gado'),
+        ('Pasto02', 'Setor secundário de pastagem para rotação do rebanho e conservação do solo'),
+        ('Cercado', 'destinada a conter animais de criação, isolar plantações ou dividir pastagens');
 `
 
 const sqlLocaisPontais: string = `

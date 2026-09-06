5b39647b99ac6f265cb21e61979a017ddfec420a
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-09-06 19:18:25 -0400

    Mdf tipo de conta logada

--- 2_semestre/lp/trabalhos/plantacao_sqlite/src/jogador/initConfiguracoesJogador.ts
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/jogador/initConfiguracoesJogador.ts
@@ -1,13 +1,12 @@
+import type { RetornoContaLogada } from "../conta/configContaLogada.ts";
 import contaLogada from "../conta/contaLogada.ts";
 
-type ContaLogada = {
-    usua_id: number,
-    usua_nome: string
-}
-
-function initConfiguracoesJogador(jogador: ContaLogada) {
+function initConfiguracoesJogador(jogador: RetornoContaLogada) {
     contaLogada.usua_id = jogador.usua_id;
     contaLogada.usua_nome = jogador.usua_nome;
+
+    // O jogador começa na casa.
+    contaLogada.localAtual = "Casa";
 }
 
 export default initConfiguracoesJogador;

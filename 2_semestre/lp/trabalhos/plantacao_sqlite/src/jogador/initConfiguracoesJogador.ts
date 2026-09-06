13b77e0c20ba3da7367ede427eaf51966b476e86
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-09-06 15:06:45 -0400

    Retirada listagem de itens do usuario

--- 2_semestre/lp/trabalhos/plantacao_sqlite/src/jogador/initConfiguracoesJogador.ts
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/jogador/initConfiguracoesJogador.ts
@@ -1,5 +1,4 @@
 import contaLogada from "../conta/contaLogada.ts";
-import listarUsuarioItem from "../db/usuario_item/listarUsuarioItem.ts";
 
 type ContaLogada = {
     usua_id: number,
@@ -9,7 +8,6 @@ type ContaLogada = {
 function initConfiguracoesJogador(jogador: ContaLogada) {
     contaLogada.usua_id = jogador.usua_id;
     contaLogada.usua_nome = jogador.usua_nome;
-    console.log(listarUsuarioItem({usua_id: contaLogada.usua_id}));
 }
 
 export default initConfiguracoesJogador;

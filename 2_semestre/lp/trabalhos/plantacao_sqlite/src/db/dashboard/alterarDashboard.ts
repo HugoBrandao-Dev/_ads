9cf01667d2de24f7a88e33337cfeafd315bd3a17
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-09-05 23:48:41 -0400

    Add alterar tabela dashboard

---
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/db/dashboard/alterarDashboard.ts
@@ -0,0 +1,27 @@
+import { AsyncLocalStorage } from "node:async_hooks";
+import db from "../conexao.ts";
+
+function prepararSQL(operacao: number): string {
+    const operacaoSQL: string = operacao > 0 ? `+ ${ operacao }` : `- ${ Math.abs(operacao) }`;
+
+    const sql: string = `
+        UPDATE dashboard
+        SET dash_qt_inicializacoes = dash_qt_inicializacoes ${ operacaoSQL };
+    `;
+
+    return sql;
+}
+
+type AlterarDashboard = {
+    operacao: number
+}
+
+function alterarDashboard(alterarDashboard: AlterarDashboard): unknown {
+    const sql: string = prepararSQL(alterarDashboard.operacao);
+
+    const alterar = db.prepare(sql);
+
+    return alterar.run();
+}
+
+export default alterarDashboard;
--- 2_semestre/lp/trabalhos/plantacao_sqlite/src/db/dashboard/ddl.ts
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/db/dashboard/ddl.ts
@@ -1,6 +1,7 @@
 import db from "../conexao.ts";
 import listarDashboard from "./listarDashboard.ts";
 import criarDashboard from "./criarDashboard.ts";
+import alterarDashboard from "./alterarDashboard.ts";
 
 const sql: string = `
     CREATE TABLE IF NOT EXISTS dashboard (
@@ -10,8 +11,16 @@ const sql: string = `
 
 function exec(): void {
     db.exec(sql);
+
+    // Verifica se é a primeira vez que o banco é criado
     if (listarDashboard().length === 0) {
+
+        // Cria uma dashboard com inicalização igual a 1
         criarDashboard();
+    } else {
+
+        // Modifica o campo de inicalização para + 1
+        alterarDashboard({operacao: 1});
     }
 }
 

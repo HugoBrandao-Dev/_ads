e4c1057f5c96e17a65e864f06d4ef729e305f200
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-09-05 21:04:07 -0400

    Add mais localizacoes padrao.

--- 2_semestre/lp/trabalhos/plantacao_sqlite/src/db/localizacao/dml.ts
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/db/localizacao/dml.ts
@@ -12,8 +12,28 @@ const sql: string = `
         ('Pasto 2', 'Setor secundário de pastagem para rotação do rebanho e conservação do solo');
 `
 
+const sqlLocaisPontais: string = `
+    INSERT INTO localizacao
+        (loca_localizacao, loca_descricao)
+    VALUES
+        ('Porteira', 'Estrutura de madeira ou metal usada para delimitar e controlar o acesso a entradas, pastos e cercados'),
+        ('Torneira', 'Dispositivo de controle do fluxo de água instalado em pontos estratégicos para irrigação, limpeza e consumo'),
+        ('Macieira', 'Árvore frutífera cultivada no pomar para a produção e colheita de maçãs'),
+        ('Lareira', 'Estrutura de alvenaria destinada ao acendimento de fogo para aquecimento do ambiente e conforto térmico');
+`;
+
+const sqlMoveis: string = `
+    INSERT INTO localizacao
+        (loca_localizacao, loca_descricao)
+    VALUES
+        ('Caixote pequeno', 'Recipiente compacto de madeira ou plástico utilizado para o transporte e armazenamento de pequenos suprimentos ou ferramentas'),
+        ('Caixote grande', 'Recipiente de grande porte adequado para o acondicionamento, organização e transporte de colheitas volumosas ou equipamentos');
+`;
+
 function exec() {
     db.exec(sql);
+    db.exec(sqlLocaisPontais);
+    db.exec(sqlMoveis);
 }
 
 export default exec;

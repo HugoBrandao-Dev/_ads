71dad8c42f1eee2376a57f641db0d474cf19ab69
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-09-07 19:20:56 -0400

    Add tratamento para item acumulativo

--- 2_semestre/lp/trabalhos/plantacao_sqlite/src/jogador/acoes/pegarItens.ts
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/jogador/acoes/pegarItens.ts
@@ -9,6 +9,7 @@ import type { RetornoLocalizacao } from "../../db/localizacao/configLocalizacao.
 import criarUsuarioItem from "../../db/usuario_item/criarUsuarioItem.ts";
 import alterarUsuarioItem from "../../db/usuario_item/alterarUsuarioItem.ts";
 import alterarLocalizacaoItemQtd from "../../db/localizacao_item/alterarLocalizacaoItemQtd.ts";
+import buscarUsuarioItem from "../../db/usuario_item/buscarUsuarioItem.ts";
 
 function pegarItens(itensID: number[]): void {
     const localAtual: RetornoLocalizacao[] = buscarLocalizacaoPeloNome({loca_localizacao: estado.ambienteAtual!});
@@ -16,17 +17,30 @@ function pegarItens(itensID: number[]): void {
 
     itensID.forEach(item_id => {
         const itemRegistro: RetornoLocalizacaoItem = buscarLocalizacaoItem({ loca_id, item_id })[0]!;
+
+        // Verifica se só há UM item no db
         if (itemRegistro.loit_quantidade === 1) {
+
+            // Excluir o item do sistema
             if (excluirLocalizacaoItem({loca_id, item_id})) {
+
+                // Registra o item no inventório do jogador
                 criarUsuarioItem({usua_id: contaLogada.usua_id, item_id });
             }
-        } /*
-        else {
+        } else {
+
+            // Retira uma unidade do item do db
             if (alterarLocalizacaoItemQtd({loca_id, item_id, valor: -1})) {
-                alterarUsuarioItem({usua_id: contaLogada.usua_id, item_id}, 1);
+
+                if (buscarUsuarioItem({usua_id: contaLogada.usua_id, item_id}).length === 0) {
+                    // Registra o item no inventório do jogador
+                    criarUsuarioItem({usua_id: contaLogada.usua_id, item_id });
+                } else {
+                    // Adiciona uma unidade do item no inventário do jogador
+                    alterarUsuarioItem({usua_id: contaLogada.usua_id, item_id}, 1);
+                }
             }
         }
-            */
     });
 }
 

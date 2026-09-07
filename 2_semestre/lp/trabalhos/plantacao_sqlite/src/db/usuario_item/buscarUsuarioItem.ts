c6e1f21a94f2674194a502a02a097e6fc9645dcb
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-09-06 23:16:51 -0400

    Mdf tipo usuario_item

--- 2_semestre/lp/trabalhos/plantacao_sqlite/src/db/usuario_item/buscarUsuarioItem.ts
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/db/usuario_item/buscarUsuarioItem.ts
@@ -1,18 +1,13 @@
 import db from "../conexao.ts";
+import type { ParametroUsuarioItem, RetornoUsuarioItem } from "./configUsuarioItem.ts";
 
 const sql: string = `
     SELECT * FROM usuario_item AS ui WHERE ui.usua_id = ? AND ui.item_id = ?;
 `;
 
-type UsuarioItemID = {
-    usua_id: number,
-    item_id: number
-}
-
-function buscarUsuarioItem(usuarioItemID: UsuarioItemID): unknown {
+function buscarUsuarioItem(usuarioItem: ParametroUsuarioItem): RetornoUsuarioItem[] {
     const select = db.prepare(sql);
-
-    return select.all(usuarioItemID.usua_id, usuarioItemID.item_id);
+    return select.all(usuarioItem.usua_id, usuarioItem.item_id) as RetornoUsuarioItem[];
 }
 
 export default buscarUsuarioItem;

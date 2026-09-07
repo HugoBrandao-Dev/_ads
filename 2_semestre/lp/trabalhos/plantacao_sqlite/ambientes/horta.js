6460e3216f977b4700eb31a5cc44a3587fd154ac
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-09-07 19:21:31 -0400

    Mdf item minhoca

--- 2_semestre/lp/trabalhos/plantacao_sqlite/ambientes/horta.js
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/ambientes/horta.js
@@ -18,16 +18,7 @@ export default {
             }
             return "Eita!! Você precisa de um regador para regar seus tomates.";
         } },
-        "4": { texto: "Inspecionar a terra", arte: "minhoca", acao: () => {
-            escreva("Você mexe na terra e encontra uma minhoca gigante.", "yellow");
-            escreva("[1] Colocar na terra de novo", "green");
-            escreva("[2] Guardar no bolso para usar de isca", "green");
-            
-            const escolha = leia("\nO que vai fazer com ela? > ");
-            if (escolha === "1") return "A minhoca voltou para a terra. Ótimo para a saúde do solo!";
-            if (escolha === "2") return "Você guardou a minhoca no bolso. Eca!";
-            return "Você ficou encarando a minhoca até ela fugir.";
-        } },
+        "4": itensJogo["Minhoca"],
         "5": { texto: "Arrancar ervas daninhas", arte: "grama", acao: () => itensJogo["Enxada"]["usar"]["Grama"]() },
         "6": { texto: "Colher tomates maduros", arte: "tomate", acao: () => "Você encontrou alguns tomates vermelhos e suculentos. Parecem deliciosos!" },
         "7": { texto: "Verificar o espantalho", arte: "espantalho", acao: () => "O espantalho está um pouco torto e com um chapéu engraçado. Parece que está fazendo um bom trabalho, nenhum pássaro por perto." },
--- 2_semestre/lp/trabalhos/plantacao_sqlite/src/items/itensJogo.ts
+++ 2_semestre/lp/trabalhos/plantacao_sqlite/src/items/itensJogo.ts
@@ -245,6 +245,34 @@ const itensJogo: object = {
 
             return "Você se afastou da torneira";
         }
+    },
+    "Minhoca": { 
+        texto: "Inspecionar a terra", 
+        arte: "minhoca", 
+        acao: () => {
+            const itensRequerido: string[] = ["Minhoca"];
+
+            const itensRegistros: ItemRetorno[] = itensRequerido.map(i => {
+                return buscarItemPeloNome({item_nome: i})[0] as ItemRetorno;
+            });
+            const itensID: number[] = itensRegistros.map(i => i.item_id);
+
+            const temItens: boolean = temItensRequeridos(itensRegistros);
+
+            escreva("Você mexe na terra e encontra uma minhoca gigante.", "yellow");
+            escreva("[1] Colocar na terra de novo", "green");
+            escreva("[2] Guardar no bolso para usar de isca", "green");
+            
+            const escolha = leia("\nO que vai fazer com ela? > ");
+            if (escolha === "1") return "A minhoca voltou para a terra. Ótimo para a saúde do solo!";
+            if (escolha === "2") {
+
+                pegarItens(itensID);                
+                return "Você guardou a minhoca no bolso. Eca!";
+
+            }
+            return "Você ficou encarando a minhoca até ela fugir.";
+        }
     }
 }
 
